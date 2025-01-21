fe0489d08609b33ed5e750bc0ed28767eefd4755
julphilruss <138808953+julphilruss@users.noreply.github.com>
2025-01-21 11:55:40 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+public class Song
+{
+   
+    public String Kuenstler;
+    public String Titel;
+    public float Dauer;
 
+   
+    public Song(String Kuenstler, String Titel, float Dauer)
+    {
+        
+        
+
+    }
+}
