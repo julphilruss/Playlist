0d7dfcf916ca9e00efd245d0d613a70ab8a91732
julphilruss <138808953+julphilruss@users.noreply.github.com>
2025-01-21 12:20:11 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 public class Song
 {
-   
     public String Kuenstler;
     public String Titel;
-    public float Dauer;
+    public float Dauer; //in sec
 
-   
     public Song(String Kuenstler, String Titel, float Dauer)
     {
         this.Kuenstler = Kuenstler;
         this.Titel = Titel;
         this.Dauer = Dauer;
-        
-        
-
+    }
+    
+    public String getTitel() {
+        return Titel;
+    }
+    
+    public String getKuenstler() {
+        return Kuenstler;
+    }
+    
+    public float getDauer() {
+        return Dauer;
+    }
+    
+    public String all() {
+        return Titel + " von " + Kuenstler+ " (Dauer in Min: " + Dauer / 60+")";
     }
 }
